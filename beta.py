b1a817f56f8d8032d2580871ac5d13598b42c3cc
WidovV <seba9188@elev.tec.dk>
2024-08-21 09:25:40 +0200

    Updates

--- beta.py
+++ beta.py
@@ -8,39 +8,36 @@ from base_person import base_person
 import os
 import asyncio
 
-# Convert the following code to classes
-# {'items': [{'subjects': [], 'title': "", 'aliases': [], 'details': ""}]}
 
-
+wanted_list_data = None
 async def fetch_fbi_wanted_list():
+    global wanted_list_data
     while True:
         print("Fetching FBI wanted list...")
         response = requests.get("https://api.fbi.gov/wanted/v1/list")
         try:
-            global value
-            value = response.json()
+            wanted_list_data = response.json()
             print("Fetched FBI wanted list")
         except json.decoder.JSONDecodeError as e:
-            print("Error decoding JSON")
+            await ("Error decoding JSON")
             # print(response.text)
             print(e)
             os.abort()
         await write_to_csv()
         await asyncio.sleep(60 * 60 * 24) # 24 hours
 
-value: any = None
 
 
 async def write_to_csv():
 
-    ids = [item['uid'] for item in value['items']]
-    names = [item['title'] for item in value['items']]
-    subjects = [item['subjects'] for item in value['items']]
-    aliases =  [item['aliases'] for item in value['items']]
-    details =  [item['details'] for item in value['items']]
+    ids = [item['uid'] for item in wanted_list_data['items']]
+    names = [item['title'] for item in wanted_list_data['items']]
+    subjects = [item['subjects'] for item in wanted_list_data['items']]
+    aliases =  [item['aliases'] for item in wanted_list_data['items']]
+    details =  [item['details'] for item in wanted_list_data['items']]
 
 
-    with open('fbi-wanted_list.csv', mode='w') as file:
+    with open('fbi_wanted_list.csv', mode='w') as file:
         writer = csv.writer(file)
 
         # Write the header row
@@ -76,6 +73,7 @@ def show_gang_members():
     print(f"gang member: {gang_members[choice].fornavn} {gang_members[choice].efternavn} | gang name: {gang_members[choice].gang_name}")
     gang_name = input(f"Enter the gang {gang_members[choice].fornavn} is a part of: ")
     gang_members[choice].gang_name = gang_name
+    update_person_details(gang_members[choice])
     print("Saved!")
     main()
 
@@ -84,7 +82,7 @@ def show_missing_persons():
     people = get_people("missing person")
     print(f"length of people: {len(people)}")
     missing_persons = [convert_to_child(person, missing_person) for person in people]
-            
+
     for i, person in enumerate(missing_persons):
         print(f"[{i}] Missing person: {person.fornavn} {person.efternavn} | Last seen: {person.last_seen}")
 
@@ -93,13 +91,14 @@ def show_missing_persons():
     print(f"Selected missing person: {missing_persons[choice].fornavn} {missing_persons[choice].efternavn}")
     last_seen = input(f"Enter the last seen location of {missing_persons[choice].fornavn}: ")
     missing_persons[choice].last_seen = last_seen
+    update_person_details(missing_persons[choice])
     print("Saved!")
     main()
 
 
 def get_people(subj: str) -> list[base_person]:
     persons: list[base_person] = []
-    for values in value['items']:
+    for values in wanted_list_data['items']:
         for subjects in values['subjects']:
             if subj.lower() not in subjects.lower():
                 continue
@@ -111,29 +110,38 @@ def get_people(subj: str) -> list[base_person]:
 
     return persons
 
+def update_person_details(person: any):
+    for values in wanted_list_data['items']:
+        if values['uid'] == person.id:
+            if isinstance(person, gang_member):
+                 values['gang_name'] = person.gang_name
+            elif isinstance(person, missing_person):
+                values['last_seen'] = person.last_seen
+            
+            return
+        
 
 def convert_to_child(obj, valueType: type):
-    value = valueType()
-    value.id = obj.id
-    value.fornavn = obj.fornavn
-    value.efternavn = obj.efternavn
-    return value
+    valueType = valueType()
+    valueType.id = obj.id
+    valueType.fornavn = obj.fornavn
+    valueType.efternavn = obj.efternavn
+    return wanted_list_data
 
 async def main():
-    await fetch_fbi_wanted_list()
+    asyncio.create_task(fetch_fbi_wanted_list())
 
-
-    choice = show_menu()
-    if choice == 1:
-        show_missing_persons()
-    elif choice == 2:
-        show_gang_members()
-    elif choice == 3:
-        print("Goodbye!")
-        os.abort()
-    else:
-        os.system("cls")
-        main()
+    await asyncio.sleep(0)
+    while True:
+        choice = show_menu()
+        if choice == 1:
+            show_missing_persons()
+        elif choice == 2:
+            show_gang_members()
+        elif choice == 3:
+            break
+        else:
+            print("Invalid choice")
 
 if __name__ == "__main__":
     asyncio.run(main())
--- datastructures.py
+++ datastructures.py
@@ -1,27 +1,2 @@
-import math
-
-def detect_ranges(l1):
-    ranges = []
-    l1.sort()
-    start = l1[0]
-    end = l1[0]
-    for i in range(1, len(l1)):
-        if l1[i] - l1[i - 1] == 1:
-            end = l1[i]
-        else:
-            if start == end:
-                ranges.append(str(start))
-            else:
-                ranges.append(f"({start},{end + 1})")
-            start = l1[i]
-            end = l1[i]
-    if start == end:
-        ranges.append(str(start))
-    else:
-        ranges.append(f"({start},{end + 1})")
-    return ranges
-
-
-
 if __name__ == "__main__":
     print("\nDag 2 øvelse datastruktur begreber\n")
